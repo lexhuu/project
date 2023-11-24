3b04d47b4b4f3a462d5c904798e00e98e3c4a77e
lexhuu <137170082+lexhuu@users.noreply.github.com>
2023-11-24 15:42:25 +0530

    Add files via upload

--- signup/signup/signup/settings.py
+++ signup/signup/signup/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'signup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIRS],
+        'DIRS': [TEMPLATES_DIRS,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- signup/signup/signup/urls.py
+++ signup/signup/signup/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sign import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.user_login,name='login'),
+    path('logout/',views.user_logout,name='logout'),
+
 ]
